b985f6288f5db26fcd660eed87a3fb596bc48ff9
Shashank Joshi <shashankjoshi368@gmail.com>
2024-07-21 19:29:46 -0400

    Chats Screen Added

---
+++ app/src/main/java/edu/northeastern/numad24su_plateperfect/User.java
@@ -0,0 +1,12 @@
+package edu.northeastern.numad24su_plateperfect;
+
+public class User {
+    String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername(){return username;}
+
+}
