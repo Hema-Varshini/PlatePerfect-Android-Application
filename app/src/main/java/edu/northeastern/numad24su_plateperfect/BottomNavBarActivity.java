42887d23f4e0420511f500f69d7f4af589611548
narodeSiddhant <siddhantnarode@gmail.com>
2024-08-02 01:25:30 -0400

    FP - 06 - nav menu updated

--- app/src/main/java/edu/northeastern/numad24su_plateperfect/BottomNavBarActivity.java
+++ app/src/main/java/edu/northeastern/numad24su_plateperfect/BottomNavBarActivity.java
@@ -1,5 +1,6 @@
 package edu.northeastern.numad24su_plateperfect;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,18 +8,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class BottomNavBarActivity extends AppCompatActivity {
+import edu.northeastern.numad24su_plateperfect.databinding.ActivityBottomNavBarBinding;
 
+public class  BottomNavBarActivity extends AppCompatActivity {
+    ActivityBottomNavBarBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_bottom_nav_bar);
+        binding =  ActivityBottomNavBarBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item ->{
+            if(item.getItemId() == R.id.cookMenu){
+                replaceFragment(new CookFragment());
+            } else if (item.getItemId() == R.id.homeMenu) {
+                replaceFragment(new HomeFragment());
+            }
+            else {
+                replaceFragment(new ProfileFragment());
+            }
+
+            return true;
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frgment_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
