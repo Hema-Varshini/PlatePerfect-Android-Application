46ddc394e433321ab39bec88bc2503bbae7b7382
narodeSiddhant <siddhantnarode@gmail.com>
2024-08-02 01:24:27 -0400

    FP - 04 - child and parent recyclerview Added

---
+++ app/src/main/java/edu/northeastern/numad24su_plateperfect/rvChildModelClass.java
@@ -0,0 +1,10 @@
+package edu.northeastern.numad24su_plateperfect;
+
+public class rvChildModelClass {
+    int image;
+    public rvChildModelClass(int image){
+        this.image=image;
+    }
+
+
+}
